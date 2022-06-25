6941dfab549c5af8ba93ddab8e5ac65420a0f7ac
AliRoshan-1382 <ar040465@gmail.com>
2022-06-25 11:11:43 +0430

    Update register.php

--- server/register.php
+++ server/register.php
@@ -195,16 +195,7 @@ foreach ($_REQUEST as $key => $value)
     
             if (mysqli_num_rows($result)  ===  0) 
             {
-                $pass=password_hash($password , PASSWORD_DEFAULT );
-                
-                $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
-
-                if (mysqli_query($connection, $sql)) 
-                {
-                    $json=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
-                    $out=json_encode($json);
-                    echo $out;
-                } 
+                insert($password , $fname , $lname , $email , $connection , $tel);
             }
             else
             {
@@ -214,28 +205,8 @@ foreach ($_REQUEST as $key => $value)
         
                 if (mysqli_num_rows($result) != 0 ) 
                 {
-                    $result=mysqli_fetch_all($result , MYSQLI_ASSOC);  
-
-                    foreach ($result as $o) 
-                    {
-                        $email1=$o['email'];
-                        $tel1=$o['tel'];
-
-                        if($email1 == $email) 
-                        {
-                            $messagg=array('status'=>FALSE , 'data'=>"ایمیل شما تکراری است");
-                            $ppaya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-                            echo $ppaya;
-                            return;
-                        }
-                        if($tel1 == $tel) 
-                        {
-                            $messagg=array('status'=>FALSE , 'data'=>"شماره تلفن شما تکراری است");
-                            $paya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION| JSON_UNESCAPED_SLASHES| JSON_UNESCAPED_UNICODE);
-                            echo $paya;
-                            return;
-                        }
-                    }
+                    $result=mysqli_fetch_all($result , MYSQLI_ASSOC);
+                    check_email_tel($result , $email , $tel);  
                 }
             } 
         }
