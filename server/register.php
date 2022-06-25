e6353c27173f4ee6a39142f335e1677f3632f3ca
AliRoshan-1382 <ar040465@gmail.com>
2022-06-25 10:06:37 +0430

    Update register.php

--- server/register.php
+++ server/register.php
@@ -1,15 +1,13 @@
 <?php
-// echo 5657574;
-
-require_once ("function.php");
-
+include ("function.php");
 include "config.php";
-// exit;
+
+
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Credentials: true");
-header("Content-Type:application/json");
+ header("Access-Control-Allow-Headers: access");
+  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  header("Access-Control-Allow-Credentials: true");
+ header("Content-Type:application/json");
 header('Content-Type: text/html; charset=utf-8');
 
 
@@ -58,14 +56,72 @@ foreach ($_REQUEST as $key => $value)
             $query = "SELECT * FROM register";
             $result = mysqli_query($connection, $query);
     
-            if (mysqli_num_rows($result) == 0) 
+            if (mysqli_num_rows($result)  ==  0) 
             {
-                first_insert($connection , $fname , $lname , $email , $tel , $password );
+                // first_insert($connection , $fname , $lname , $email , $tel , $password );
+                $pass=password_hash($password , PASSWORD_DEFAULT );
+                
+                $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
+
+                if (mysqli_query($connection, $sql)) 
+                {
+                    $json=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
+                    $out=json_encode($json);
+                    echo $out;
+                } 
             }
             else
             {
-                select($connection);
-                check_information($result,$email,$tel);
+                // select($connection);
+                // $check=check_information($result,$email,$tel);
+                // if ($check) 
+                // {
+                //     second_insert($fname, $lname , $email , $email1 , $tel ,$tel1 , $password , $connection);
+                // }
+
+                $sql1 = " SELECT  `tel`,`email` FROM register";
+            
+                $result=mysqli_query($connection,$sql1);
+        
+                if (mysqli_num_rows($result) != 0 ) 
+                {
+                    $result=mysqli_fetch_all($result , MYSQLI_ASSOC);  
+
+                    foreach ($result as $o) 
+                    {
+                            $email1=$o['email'];
+                            $tel1=$o['tel'];
+        
+                                if($email1 == $email) 
+                                {
+                                    $messagg=array('status'=>FALSE , 'data'=>"ایمیل شما تکراری است");
+                                    $ppaya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+                                    echo $ppaya;
+                                    return;
+                                }
+                                if($tel1 == $tel) 
+                                {
+                                    $messagg=array('status'=>FALSE , 'data'=>"شماره تلفن شما تکراری است");
+                                    $paya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION| JSON_UNESCAPED_SLASHES| JSON_UNESCAPED_UNICODE);
+                                    echo $paya;
+                                    return;
+                                }
+                    }
+
+                    if ( $email1 !== $email  &&  $tel1 !== $tel) 
+                    {
+                        $pass=password_hash($password , PASSWORD_DEFAULT );
+            
+                        $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
+                    
+                        if (mysqli_query($connection, $sql)) 
+                        {
+                            $messagg=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
+                            $paya=json_encode($messagg);
+                            echo $paya;
+                        } 
+                    }
+                }
             } 
         }
         else 
