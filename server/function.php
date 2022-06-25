dcd66d5ec4c128d55c913fea6d21209f4f4ea6e1
AliRoshan-1382 <ar040465@gmail.com>
2022-06-25 10:59:16 +0430

    Update function.php

--- server/function.php
+++ server/function.php
@@ -1,74 +1,74 @@
 <?php
 
-    function first_insert($connection , $fname , $lname , $email , $tel , $password )
-    {
-        $pass=password_hash($password , PASSWORD_DEFAULT );
+    // function first_insert($connection , $fname , $lname , $email , $tel , $password )
+    // {
+    //     $pass=password_hash($password , PASSWORD_DEFAULT );
         
-        $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
+    //     $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
 
-        if (mysqli_query($connection, $sql)) 
-        {
-            $json=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
-            $out=json_encode($json);
-            echo $out;
-        } 
-    }
+    //     if (mysqli_query($connection, $sql)) 
+    //     {
+    //         $json=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
+    //         $out=json_encode($json);
+    //         echo $out;
+    //     } 
+    // }
 
-    function select($connection)
-    {
+    // function select($connection)
+    // {
 
-        $sql1 = " SELECT  `tel`,`email`,`password` FROM register";
+    //     $sql1 = " SELECT  `tel`,`email`,`password` FROM register";
             
-        $result=mysqli_query($connection,$sql1);
+    //     $result=mysqli_query($connection,$sql1);
 
-        if (mysqli_num_rows($result) !=0 ) 
-        {
-            $result=mysqli_fetch_all($result , MYSQLI_ASSOC);
+    //     if (mysqli_num_rows($result) !=0 ) 
+    //     {
+    //         $result=mysqli_fetch_all($result , MYSQLI_ASSOC);
             
-        }
+    //     }
 
-    }
+    // }
 
-    function check_information($result,$email,$tel)
-    {
-            foreach ($result as $o) 
-            {
-                    $email1=$o['email'];
-                    $tel1=$o['tel'];
+    // function check_information($result,$email,$tel)
+    // {
+    //         foreach ($result as $o) 
+    //         {
+    //                 $email1=$o['email'];
+    //                 $tel1=$o['tel'];
 
-                        if($email1 == $email) 
-                        {
-                            $messagg=array('status'=>FALSE , 'data'=>"ایمیل شما تکراری است");
-                            $ppaya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
-                            echo $ppaya;
-                            return;
-                        }
-                        if($tel1 == $tel) 
-                        {
-                            $messagg=array('status'=>FALSE , 'data'=>"شماره تلفن شما تکراری است");
-                            $paya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION| JSON_UNESCAPED_SLASHES| JSON_UNESCAPED_UNICODE);
-                            echo $paya;
-                            return;
-                        }
-            }
-    }
+    //                     if($email1 == $email) 
+    //                     {
+    //                         $messagg=array('status'=>FALSE , 'data'=>"ایمیل شما تکراری است");
+    //                         $ppaya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+    //                         echo $ppaya;
+    //                         return;
+    //                     }
+    //                     if($tel1 == $tel) 
+    //                     {
+    //                         $messagg=array('status'=>FALSE , 'data'=>"شماره تلفن شما تکراری است");
+    //                         $paya=json_encode($messagg , JSON_PRESERVE_ZERO_FRACTION| JSON_UNESCAPED_SLASHES| JSON_UNESCAPED_UNICODE);
+    //                         echo $paya;
+    //                         return;
+    //                     }
+    //         }
+    // }
 
-    function second_insert($fname, $lname , $email , $email1 , $tel ,$tel1 , $password , $connection)
-    {
-        if ( $email1!=$email  &&  $tel1!=$tel) 
-        {
-            $pass=password_hash($password , PASSWORD_DEFAULT );
+    // function second_insert($fname, $lname , $email , $email1 , $tel ,$tel1 , $password , $connection)
+    // {
+    //     if ( $email1!=$email  &&  $tel1!=$tel) 
+    //     {
+    //         $pass=password_hash($password , PASSWORD_DEFAULT );
 
-            $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
+    //         $sql = "INSERT INTO register (`fname` , `lname` , `email` , `tel` , `password`) VALUES ('$fname' , '$lname' , '$email' , '$tel' , '$pass')";
         
-            if (mysqli_query($connection, $sql)) 
-            {
-                $messagg=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
-                $paya=json_encode($messagg);
-                echo $paya;
-            } 
-        }
-    }
+    //         if (mysqli_query($connection, $sql)) 
+    //         {
+    //             $messagg=array('status'=>TRUE , 'data'=>"اطلاعات کاربری شما با موفقیت ثبت شد");
+    //             $paya=json_encode($messagg);
+    //             echo $paya;
+    //         } 
+    //     }
+    // }
 
 
     //_______________________________________________________________________LOGIN_______________________________________________________________________
