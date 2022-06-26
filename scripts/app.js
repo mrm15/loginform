920c8aaf683adca25bbc9efeecfff6d237b61ca4
Mobin <105428684+mobin-ghaemi@users.noreply.github.com>
2022-06-26 14:11:47 +0430

    10Line Codes

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,10 @@
-// Get Register Form
 const registerForm = $("form#register"),loginForm = $("form#login");
-// Check Exist RegisterForm
-if (registerForm[0]) {
+if (registerForm[0]) 
   registerForm[0].addEventListener("submit", e => DUPLICATE(e, "register", registerForm));
-} else if (loginForm[0]) {
+  else if (loginForm[0]) 
   loginForm[0].addEventListener("submit", e => DUPLICATE(e, "login", loginForm));
-}
 function DUPLICATE(e, page, form) {
   e.preventDefault();
-  // Call Register.php (main back-end code) fetch
-  page === "register" ? fetch(`server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}`) :
-  fetch(`../server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}`);
+  fetch(page === "register" ? `server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}` : 
+  `../server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}`)
 }
