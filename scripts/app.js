1da5ffecffd48575cf34883dbeb683b1bf259f28
Mobin <105428684+mobin-ghaemi@users.noreply.github.com>
2022-06-26 15:23:23 +0430

    Add Captcha Validation

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,40 @@
 const registerForm = $("form#register"),loginForm = $("form#login");
-if (registerForm[0]) 
-  registerForm[0].addEventListener("submit", e => DUPLICATE(e, "register", registerForm));
-else if (loginForm[0]) 
-  loginForm[0].addEventListener("submit", e => DUPLICATE(e, "login", loginForm));
+
+const captcha = document.querySelector("#captcha");
+const userCode = document.querySelector("#userCode");
+
+const captchaCode = Math.floor(Math.random() * 123456);
+captcha.value = captchaCode;
+
+if (registerForm[0])
+  registerForm[0].addEventListener("submit", (e) =>
+    DUPLICATE(e, "register", registerForm)
+  );
+else if (loginForm[0])
+  loginForm[0].addEventListener("submit", (e) =>
+    DUPLICATE(e, "login", loginForm)
+  );
 function DUPLICATE(e, page, form) {
   e.preventDefault();
   const information = JSON.stringify(form.serializeArray());
-  fetch(page === "register" ? 
-  `server/${page}.php?${page}=${information}` : 
-  `../server/${page}.php?${page}=${information}`)
-  .then(response => response.json())
-  .then(response => { 
+
+  if (captcha.value === userCode.value) {
+    fetch(
+      page === "register"
+        ? `server/${page}.php?${page}=${information}`
+        : `../server/${page}.php?${page}=${information}`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        Swal.fire({
+          icon: "info",
+          title: response.data,
+        });
+      });
+  } else {
     Swal.fire({
-      icon: 'info',
-      title: response.data,
-    })
-  }) 
+      icon: "error",
+      title: "درست وارد کن بچه :|",
+    });
+  }
 }
