fd67b0c41c1a2090e6c69b8046711dbcc8826816
Mobin <105428684+mobin-ghaemi@users.noreply.github.com>
2022-06-26 14:33:58 +0430

    Add Swal

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,19 @@
 const registerForm = $("form#register"),loginForm = $("form#login");
 if (registerForm[0]) 
   registerForm[0].addEventListener("submit", e => DUPLICATE(e, "register", registerForm));
-  else if (loginForm[0]) 
+else if (loginForm[0]) 
   loginForm[0].addEventListener("submit", e => DUPLICATE(e, "login", loginForm));
 function DUPLICATE(e, page, form) {
   e.preventDefault();
-  fetch(page === "register" ? `server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}` : 
-  `../server/${page}.php?${page}=${JSON.stringify(form.serializeArray())}`)
+  const information = JSON.stringify(form.serializeArray());
+  fetch(page === "register" ? 
+  `server/${page}.php?${page}=${information}` : 
+  `../server/${page}.php?${page}=${information}`)
+  .then(response => response.json())
+  .then(response => { 
+    Swal.fire({
+      icon: 'info',
+      title: response.data,
+    })
+  }) 
 }
