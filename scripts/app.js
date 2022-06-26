0abf309c22fcde92e5045af77bbfe9ea9002b16d
Mobin <105428684+mobin-ghaemi@users.noreply.github.com>
2022-06-26 11:33:19 +0430

    Better-2 Codes

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,9 @@
 const registerForm = $("form#register"),loginForm = $("form#login");
 // Check Exist RegisterForm
 if (registerForm[0]) {
-  registerForm[0].addEventListener("submit", (e) => {
-    DUPLICATE(e, "register", registerForm);
-  });
+  registerForm[0].addEventListener("submit", e => DUPLICATE(e, "register", registerForm));
 } else if (loginForm[0]) {
-  loginForm[0].addEventListener("submit", (e) => {
-    DUPLICATE(e, "login", loginForm);
-  });
+  loginForm[0].addEventListener("submit", e => DUPLICATE(e, "login", loginForm));
 }
 function DUPLICATE(e, page, form) {
   e.preventDefault();
