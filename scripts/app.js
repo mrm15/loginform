a7f6ec1c86cae13c6bfd1a6d86002426f922e7af
Mobin <105428684+mobin-ghaemi@users.noreply.github.com>
2022-06-26 16:31:24 +0430

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -47,24 +47,26 @@ function DUPLICATE(e, page, form) {
   e.preventDefault();
   const information = JSON.stringify(form.serializeArray());
 
-  if (captcha.value === userCode.value) {
-    fetch(
-      page === "register"
-        ? `server/${page}.php?${page}=${information}`
-        : `../server/${page}.php?${page}=${information}`
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        Swal.fire({
-          icon: "info",
-          title: response.data,
+  if (captcha) {
+    if (captcha.value === userCode.value) {
+      fetch(
+        page === "register"
+          ? `server/${page}.php?${page}=${information}`
+          : `../server/${page}.php?${page}=${information}`
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          Swal.fire({
+            icon: "info",
+            title: response.data,
+          });
         });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "درست وارد کن بچه :|",
       });
-  } else {
-    Swal.fire({
-      icon: "error",
-      title: "درست وارد کن بچه :|",
-    });
+    }
   }
 }
 
